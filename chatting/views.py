c120711b7c1fd92f4fd21cb789424db4086b590a
john-denver <denverbogonko78@gmail.com>
2023-06-19 04:51:24 +0300

    social media django application

--- chatting/views.py
+++ chatting/views.py
@@ -64,24 +64,37 @@ def send_chat(request):
         pass
 
 
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.shortcuts import render
+
+
 @login_required
 def user_search(request):
     query = request.GET.get('q')
-    context = {
+    context = {}
 
-    }
     if query:
-        users = User.objects.filter(Q(username__icontains=query))
+        users = User.objects.filter(username__icontains=query)
 
-        #  Pagination
+        # Pagination
         paginator = Paginator(users, 8)
         page_number = request.GET.get('page')
         users_paginator = paginator.get_page(page_number)
 
-        # when passing contexts what's inside the ' ' will be called in html templated i.e. {{ profile.user }}
+        # when passing contexts, what's inside the ' ' will be called in the HTML template i.e. {{ profile.user }}
         context = {
             'users': users_paginator
         }
+
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':  # Check if the request is AJAX
+        search_results = render_to_string('search_results.html', context)
+        return HttpResponse(search_results, content_type='text/html')
+
     return render(request, 'search.html', context)
 
 
