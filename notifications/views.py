b5560ae84aa75a854614fb3cd81408af8542ddad
john-denver <denverbogonko78@gmail.com>
2023-06-21 16:18:09 +0300

    social media django application

--- notifications/views.py
+++ notifications/views.py
@@ -1,7 +1,11 @@
+from datetime import timedelta
+
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse, JsonResponse
+from django.utils import timezone
 
 from notifications.models import Notification
 
@@ -9,7 +13,38 @@ from notifications.models import Notification
 @login_required
 def show_notifications(request):
     user = request.user
-    notifications = Notification.objects.filter(recipient=user).order_by('-timestamp')
+    all_users = User.objects.all()
+
+    # Get the current date and time
+    now = timezone.now()
+
+    # Calculate the start and end dates for "Today"
+    today_start = now.date()
+    today_end = today_start + timedelta(days=1)
+
+    # yesterday = now.date() - timedelta(days=1)
+
+    # Calculate the start and end dates for "This Week"
+    this_week_start = today_start - timedelta(days=today_start.weekday())
+    this_week_end = this_week_start + timedelta(days=7)
+
+    # Filter notifications based on their timestamps
+    today_notifications = Notification.objects.filter(
+        recipient=user,
+        timestamp__range=(today_start, today_end)
+    )
+
+    this_week_notifications = Notification.objects.filter(
+        recipient=user,
+        timestamp__range=(this_week_start, this_week_end)
+    ).exclude(timestamp__range=(today_start, today_end))
+
+    other_notifications = Notification.objects.filter(
+        recipient=user,
+        timestamp__lt=this_week_start
+    )
+
+    # Update the 'is_read' status of the notifications
     Notification.objects.filter(recipient=user, is_read=False).update(is_read=True)
 
     count_notifications = 0
@@ -19,12 +54,14 @@ def show_notifications(request):
     template = loader.get_template('notifications/notification.html')
 
     context = {
-        'notifications': notifications,
+        'all_users': all_users,
+        'today_notifications': today_notifications,
+        'this_week_notifications': this_week_notifications,
+        'other_notifications': other_notifications,
         'count_notifications': count_notifications,
     }
 
     return HttpResponse(template.render(context, request))
-
 
 @login_required
 def delete_notification(request, noti_id):
@@ -41,3 +78,23 @@ def count_notification(request):
         count_notifications = Notification.objects.filter(recipient=user, is_read=False).count()
 
     return JsonResponse({'count_notifications': count_notifications})
+
+
+# @login_required
+# def show_notifications(request):
+#     user = request.user
+#     notifications = Notification.objects.filter(recipient=user).order_by('-timestamp')
+#     Notification.objects.filter(recipient=user, is_read=False).update(is_read=True)
+#
+#     count_notifications = 0
+#     if user.is_authenticated:
+#         count_notifications = Notification.objects.filter(recipient=user, is_read=False).count()
+#
+#     template = loader.get_template('notifications/notification.html')
+#
+#     context = {
+#         'notifications': notifications,
+#         'count_notifications': count_notifications,
+#     }
+#
+#     return HttpResponse(template.render(context, request))
